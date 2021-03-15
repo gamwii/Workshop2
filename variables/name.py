d31356a48791373477065d1ffa571870f2ec266b
gamwii <77233978+gamwii@users.noreply.github.com>
2021-03-15 23:11:33 +0700

    Create name.py

---
+++ variables/name.py
@@ -0,0 +1,14 @@
+#สามารถประกาศได้
+myvar = "gam"
+my_var = "gam"
+_my_var_ = "gam"
+myVar = "gam"
+MYVAR = "gam"
+myvar2 = "gam"
+#ตัวพิมพ์ใหญ่ทั้งหมดคือตัวแปรที่ไม่ควรเปลี่ยนแปลงค่า
+
+
+#ไม่สามารถประกาศได้
+2myvar = "gam"
+my-var = "gam"
+my var = "gam"
