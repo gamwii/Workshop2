2d2c4e1d7b3dfc61c922e563f46f25526797fbe5
gamwii <77233978+gamwii@users.noreply.github.com>
2021-03-15 17:08:15 +0700

    Create for6.py

---
+++ for/for6.py
@@ -0,0 +1,11 @@
+adjectives = ["small", "big"]
+fruits = ["apple", "banana", "cherry"]
+for adjectives in adjectives:
+    for fruit in fruits:
+        print(adjectives, fruit)
+# small apple
+# small banana
+# small cherry
+# big apple
+# big banana
+# big cherry
