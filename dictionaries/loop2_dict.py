5a8a1318d6956064d896855e6767776223dcfd0a
gamwii <77233978+gamwii@users.noreply.github.com>
2021-03-15 15:44:17 +0700

    Create loop2_dict.py

---
+++ dictionaries/loop2_dict.py
@@ -0,0 +1,12 @@
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1984}
+
+# วนลูปจะได้ value ออกมา
+for key in thisdict:
+    print(thisdict[key])
+    # Output : Ford Mustang 1984
+
+
+# key = brand, model, year
+# 1 thisdict["brand"] -> Ford
+# 2 thisdict["model"] -> Mustang
+# 3 thisdict["year"] -> 1964
