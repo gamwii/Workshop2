fc0e3bcceae7009903027b8bc38023bca0a2b9b2
gamwii <77233978+gamwii@users.noreply.github.com>
2021-03-15 20:26:46 +0700

    Create Arithmetic-operators.py

---
+++ Operators/Arithmetic-operators.py
@@ -0,0 +1,10 @@
+x = 17
+y = 2
+
+print(x + y)  # Output : 19
+print(x - y)  # Output : 15
+print(x * y)  # Output : 34
+print(x / y)  # Output : 8.5
+print(x % y)  # Output : 1
+print(x ** y)  # Output : 289
+print(x // y)  # Output : 8
