2f7e305b1a6b54793e8cf5978b62302a39a3de62
gamwii <77233978+gamwii@users.noreply.github.com>
2021-03-15 23:03:53 +0700

    Create Access-Tuple.py

---
+++ tuples/Access-Tuple.py
@@ -0,0 +1,11 @@
+thislist = ("apple", "banana", "cherry")
+
+print(thislist[0])
+print(thislist[1])
+print(thislist[2])
+
+print(thislist[-1])
+print(thislist[-2])
+print(thislist[-3])
+
+print(thislist[1])
