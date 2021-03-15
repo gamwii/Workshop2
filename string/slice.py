31e45596477d74543a9ddd5691005955b448b05b
gamwii <77233978+gamwii@users.noreply.github.com>
2021-03-15 22:49:19 +0700

    Create slice.py

---
+++ string/slice.py
@@ -0,0 +1,8 @@
+string = "Hello, World!"
+
+print(string[2:5])  # Output : llo
+print(string[:5])  # Output : Hello
+print(string[2:])  # Output : llo, World!
+print(string[-5:-2])  # Output : orl
+print(string[0])  # Output : H
+print(string[-1:])  # Output : !
