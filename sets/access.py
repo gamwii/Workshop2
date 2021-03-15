bcef43d13b2fb97f661c778c931a51e4e94227f5
gamwii <77233978+gamwii@users.noreply.github.com>
2021-03-15 22:30:48 +0700

    Create access.py

---
+++ sets/access.py
@@ -0,0 +1,17 @@
+# Ex1
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+    print(x)
+
+# Output :
+# banana
+# cherry
+# apple
+
+# Ex2
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits:
+    print("Yes, apple is a fruit!")
+
+# Output : Yes, apple is a fruit!
