8f8805658f94465d1a27c663ee9423e8ca7571fd
gamwii <77233978+gamwii@users.noreply.github.com>
2021-03-15 22:22:21 +0700

    Create Membership-operators.py

---
+++ Operators/Membership-operators.py
@@ -0,0 +1,34 @@
+string = "Hello world"
+
+print("H" in string)
+# True
+
+print("hELLo" not in string)
+# True
+
+
+dictObject = {"name": "rujeongwoo", "age": "17"}
+
+print("name" in dictObject)
+# True
+
+print("rujeongwoo" in dictObject)
+# False
+
+
+number_list = [1, 2, 3]
+
+print(1 in number_list)
+# True
+
+print(5 in number_list)
+# False
+
+
+name_list = ["ruru", "woory"]
+
+print("ruru" in name_list)
+# True
+
+print("romy" in name_list)
+# False
