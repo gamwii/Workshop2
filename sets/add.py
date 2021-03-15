f48218f007c4afec6e9b401932e3d35889a55b6c
gamwii <77233978+gamwii@users.noreply.github.com>
2021-03-15 22:31:18 +0700

    Create add.py

---
+++ sets/add.py
@@ -0,0 +1,15 @@
+# EX 1
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+
+# Output : {'apple', 'orange', 'cherry', 'banana'}
+
+
+# EX 2
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+thisset.update(tropical)
+print(thisset)
+
+# Output : {'pineapple', 'apple', 'cherry', 'papaya', 'banana', 'mango'}
