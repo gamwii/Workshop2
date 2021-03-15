2f91872e90acdc90d77ba5c50e7b40f02650fe93
gamwii <77233978+gamwii@users.noreply.github.com>
2021-03-15 18:25:25 +0700

    Create Sort-list.py

---
+++ list/Sort-list.py
@@ -0,0 +1,9 @@
+# EXAMPLE 1
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)  # Output : [23, 50, 65, 82, 100]
+
+# EXAMPLE 2
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse=True)
+print(thislist)  # Output : [100, 82, 65, 50, 23]
