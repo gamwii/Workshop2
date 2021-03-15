7e79e25a697c5d23f060c07fe4b955015723f297
gamwii <77233978+gamwii@users.noreply.github.com>
2021-03-15 18:22:57 +0700

    Create Add-list.py

---
+++ list/Add-list.py
@@ -0,0 +1,9 @@
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+
+print(thislist)  # Output : ['apple', 'banana', 'cherry', 'orange']
+
+# EXAMPLE 2
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)  # Output : ['apple', 'orange', 'banana', 'cherry']
