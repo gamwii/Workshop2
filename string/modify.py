f3fb54bca7f5fc39f3324f9971a88016e3a25926
gamwii <77233978+gamwii@users.noreply.github.com>
2021-03-15 22:48:10 +0700

    Create modify.py

---
+++ string/modify.py
@@ -0,0 +1,8 @@
+string = " Hello, World! "
+
+print(string.upper())  # Output: " HELLO, WORLD!"
+print(string.lower())  # Output: "hello, world"
+print(string.strip())  # Output: "Hello, world!"
+print(string.replace("H", "J"))  # Output: "Jello,World!"
+print(string.split(","))  # Output: [' Hello', 'World! ']
+print(len(string))  # 15
