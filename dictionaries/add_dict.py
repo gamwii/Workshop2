2072c7aefbc3701d11c70620a67ea4aa61afaff6
gamwii <77233978+gamwii@users.noreply.github.com>
2021-03-15 15:34:27 +0700

    Create add_dict.py

---
+++ dictionaries/add_dict.py
@@ -0,0 +1,8 @@
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1984}
+thisdict["color"] = "red"
+print(thisdict)
+
+# use method get
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1984}
+thisdict.update({"color": "red"})
+print(thisdict)
