1bc26a7d1fe4e69529bb43e9882e04c8703765cc
gamwii <77233978+gamwii@users.noreply.github.com>
2021-03-15 23:10:44 +0700

    Create mutiple_word_name.py

---
+++ variables/mutiple_word_name.py
@@ -0,0 +1,8 @@
+# Camel Case (java scrip,node,react,go)
+myVariableName = "Anuchita Suksombat"
+
+# Pascal Case (java)
+MyVariableName = "Anuchita Suksombat"
+
+# Snake Case (คนนิยมใช้ใน python)
+my_variable_Name = "Anuchita Suksombat"
