cd3fc019afe6a6c38aeec944bcfc9becdd8267fa
gamwii <77233978+gamwii@users.noreply.github.com>
2021-03-15 20:19:52 +0700

    Create run_class.py

---
+++ module/class/run_class.py
@@ -0,0 +1,12 @@
+import class_person
+
+name = "Piyakorn Panthapin"
+age = 21
+address = "Chonburi"
+
+person = class_person.Person(name, age, address)
+
+print(person.get_name())
+print(person.get_age())
+print(person.get_address())
+print(person.get_info())
