c22489f07ee8371aafd5b58ed98006b20aba440f
gamwii <77233978+gamwii@users.noreply.github.com>
2021-03-15 23:13:35 +0700

    Create type.py

---
+++ variables/type.py
@@ -0,0 +1,11 @@
+name = "Anuchita Suksombat"
+age = 22
+weight = 45
+can_walk = True
+can_run = True
+
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(can_walk))
+print(type(can_run))
