53928983dc5a997d6400a8a137f8118a0a41a4ce
gamwii <77233978+gamwii@users.noreply.github.com>
2021-03-15 20:27:35 +0700

    Create Comparison-operators.py

---
+++ Operators/Comparison-operators.py
@@ -0,0 +1,9 @@
+x = 10
+y = 12
+
+print("x > y is", x > y)  # Output : x > y is False
+print("x < y is", x < y)  # Output : x < y is True
+print("x == y is", x == y)  # Output : x == y is False
+print("x != y is", x != y)  # Output : x != y is True
+print("x >= y is", x >= y)  # Output : x >= y is False
+print("x <= y is", x <= y)  # Output : x <= y is True
